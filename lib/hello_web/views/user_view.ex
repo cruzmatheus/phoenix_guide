2a42499280f712a1fc91741d9269677590a6e0e1
Matheus Cruz <matheus.cruz@talkdesk.com>
2022-06-28 23:52:55 +0100

    Add email to User index

--- lib/hello_web/views/user_view.ex
+++ lib/hello_web/views/user_view.ex
@@ -1,3 +1,8 @@
 defmodule HelloWeb.UserView do
   use HelloWeb, :view
+
+  alias Hello.Accounts.User
+
+  def credential_email(%User{credential: nil}), do: nil
+  def credential_email(%User{credential: credential}), do: credential.email
 end
